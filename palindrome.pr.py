bb8f19beee5906bbe643d3ca94760a6185bd04b3
Lurangel254 <emilykiswii@gmail.com>
2025-02-19 15:20:37 +0300

    Update palindrome.pr.py

--- palindrome.pr.py
+++ palindrome.pr.py
@@ -12,3 +12,11 @@ if is_palindrome(word):
     print(f"{word} is a palindrome.")
 else:
     print(f"{word} is not a palindrome.")
+
+#or
+
+word = input("Enter a word: ").lower()
+if word == word[::-1]:
+    print(f"{word} is a palindrome)
+else:
+    print(f"{word} is not a palindrome)
